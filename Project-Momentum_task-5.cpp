ea4a599d3f766cb267bd19cca13784e0b21e76b3
swayam thummar <127782240+swayamthummar@users.noreply.github.com>
2023-03-21 22:41:24 +0530

    Add files via upload

---
+++ Project-Momentum_task-5.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+main ()
+{
+	int unit,amount,surcharge;
+	printf("enter electricity unti->");
+	scanf("%d",&unit);
+	
+	if(unit<=50)
+	{
+		amount=unit*0.50;
+	}
+	else if (unit<=150)
+	{
+		amount=(unit-50)*0.75+25;
+	}
+	else if (unit<=250)
+	{
+		amount=(unit-150)*1.20+100;
+    }
+    else
+    {
+    	amount=(unit-250)*1.50+220;
+	}
+	surcharge=amount * 0.20;
+    amount += surcharge;
+    
+    printf("Total Electricity amount is-> Rs.%d",amount);
+		
+}
